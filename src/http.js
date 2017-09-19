e9480c64a5e34a4c7eae3b943eb07b7d8642d107
jacklrs <20738850+jacklrs@users.noreply.github.com>
2017-09-19 21:55:02 +0200

    Update http.js

--- src/http.js
+++ src/http.js
@@ -9,10 +9,14 @@ export default (uri, data, method='POST') => {
            if (req.status != 200) {
                 var err = new Error('HTTP Error:' + req.statusText);
                 err.status = req.status;
-                err.body = typeof err.response == 'string' ? req.response : req.response && JSON.stringify(req.response) || req.responseText;
+                err.body = typeof err.response == 'string' ? 
+                    req.response 
+                    : req.response && JSON.stringify(req.response) || req.responseText;
                 reject(err);
             } else {
-                var response = typeof req.response == 'string' ? JSON.parse(req.response) : req.response || JSON.parse(req.responseText);
+                var response = typeof req.response == 'string' ? 
+                    JSON.parse(req.response) 
+                    : req.response || JSON.parse(req.responseText);
                 resolve(response);
             }
         }
