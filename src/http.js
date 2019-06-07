06baaf75022e782070f4ff442976372b88ad2e1f
Jochen Probst <jpr@dds-mbpro-150.fritz.box>
2019-06-07 21:27:17 +0200

    update dependencies

--- src/http.js
+++ src/http.js
@@ -1,39 +1,39 @@
 export default (uri, data, method='POST') => {
-    return new Promise((resolve, reject) => {
-        var req = new XMLHttpRequest
-        req.onreadystatechange = () => {
-            if(req.readyState != 4){
-                return;
-            }
+	return new Promise((resolve, reject) => {
+		var req = new XMLHttpRequest
+		req.onreadystatechange = () => {
+			if(req.readyState != 4){
+				return
+			}
 
-           if (req.status != 200) {
-                var err = new Error('HTTP Error:' + req.statusText);
-                err.status = req.status;
-                try{
-                    err.body = typeof err.response == 'string' ?
-                        req.response
-                        : req.response && JSON.stringify(req.response)
-                        || req.responseText;
-                }catch(e){
-                    err.body = ''
-                }
-                reject(err);
-            } else {
-                if (typeof req.response == 'string') {
-                    resolve(req.response && JSON.parse(req.response) || null)
-                } else if (req.response === null || req.response) {
-                    resolve(req.response)
-                } else {
-                    resolve(req.responseText && JSON.parse(req.responseText) || null)
-                }
-            }
-        }
+			if (req.status != 200) {
+				var err = new Error('HTTP Error:' + req.statusText)
+				err.status = req.status
+				try{
+					err.body = typeof err.response == 'string' ?
+						req.response
+						: req.response && JSON.stringify(req.response)
+                        || req.responseText
+				}catch(e){
+					err.body = ''
+				}
+				reject(err)
+			} else {
+				if (typeof req.response == 'string') {
+					resolve(req.response && JSON.parse(req.response) || null)
+				} else if (req.response === null || req.response) {
+					resolve(req.response)
+				} else {
+					resolve(req.responseText && JSON.parse(req.responseText) || null)
+				}
+			}
+		}
 
-        req.open(method, uri, true)
-        req.withCredentials = true
-        req.setRequestHeader('Accept', 'application/json')
-        req.setRequestHeader('Content-Type', 'application/json')
-        req.responseType = 'json'
-        req.send(JSON.stringify(data))
-    })
+		req.open(method, uri, true)
+		req.withCredentials = true
+		req.setRequestHeader('Accept', 'application/json')
+		req.setRequestHeader('Content-Type', 'application/json')
+		req.responseType = 'json'
+		req.send(JSON.stringify(data))
+	})
 }
--- src/http.spec.js
+++ src/http.spec.js
@@ -4,95 +4,95 @@ import FakeXMLHTTPRequests from 'fakexmlhttprequest'
 
 var requests = []
 global.XMLHttpRequest = function() {
-    var r =  new FakeXMLHTTPRequests(arguments)
-    requests.push(r)
-    return r
+	var r =  new FakeXMLHTTPRequests(arguments)
+	requests.push(r)
+	return r
 }
 
 describe('httpRequest ' + new Date(), () => {
-    var response = {status:'ok'}
+	var response = {status:'ok'}
 
-    afterEach(() => {
-        requests = []
-    })
+	afterEach(() => {
+		requests = []
+	})
 
-    it('is available', () => {
-        expect(http).not.to.be.null
-    })
+	it('is available', () => {
+		expect(http).not.to.be.null
+	})
 
-    it('returns object', (done) => {
-        http('https://localhost', {}).then(
-            value => {
-                expect(value).to.deep.equal(response)
-                done()
-            }
-        ).catch(err => done(err))
-        requests[0].respond(
-            200,
-            {"Content-Type": "application/json"},
-            JSON.stringify(response)
-        )
-    })
-    
-    it('returns null string body', (done) => {
-        http('https://localhost', {}).then(
-            value => {
-                expect(value).to.be.null
-                done()
-            }
-        ).catch(err => done(err))
-        requests[0].respond(
-            200,
-            {"Content-Type": "application/json"},
-            'null'
-        )
-    })
-    it('returns empty body', (done) => {
-        http('https://localhost', {}).then(
-            value => {
-                expect(value).to.be.null
-                done()
-            }
-        ).catch(err => done(err))
-        requests[0].respond(
-            200,
-            {"Content-Type": "application/json"},
-            ''
-        )
-    })
-    it('returns null body', (done) => {
-        http('https://localhost', {}).then(
-            value => {
-                expect(value).to.be.null
-                done()
-            }
-        ).catch(err => done(err))
-        requests[0].respond(
-            200,
-            {"Content-Type": "application/json"},
-            null
-        )
-    })
+	it('returns object', (done) => {
+		http('https://localhost', {}).then(
+			value => {
+				expect(value).to.deep.equal(response)
+				done()
+			}
+		).catch(err => done(err))
+		requests[0].respond(
+			200,
+			{'Content-Type': 'application/json'},
+			JSON.stringify(response)
+		)
+	})
 
-    it('handle http errors', done => {
-        http('', {}).then(
-            value => {
-                expect(true).to.be.false
-                done()
-            },
-            (err) => {
-                expect(err).to.be.a("Error")
-                expect(err.status).to.be.a("Number")
-                expect(err.status).to.be.equal(404)
-                expect(err.body).to.be.a("String")
-                done()
-            }
-        ).catch(err => done(err))
+	it('returns null string body', (done) => {
+		http('https://localhost', {}).then(
+			value => {
+				expect(value).to.be.null
+				done()
+			}
+		).catch(err => done(err))
+		requests[0].respond(
+			200,
+			{'Content-Type': 'application/json'},
+			'null'
+		)
+	})
+	it('returns empty body', (done) => {
+		http('https://localhost', {}).then(
+			value => {
+				expect(value).to.be.null
+				done()
+			}
+		).catch(err => done(err))
+		requests[0].respond(
+			200,
+			{'Content-Type': 'application/json'},
+			''
+		)
+	})
+	it('returns null body', (done) => {
+		http('https://localhost', {}).then(
+			value => {
+				expect(value).to.be.null
+				done()
+			}
+		).catch(err => done(err))
+		requests[0].respond(
+			200,
+			{'Content-Type': 'application/json'},
+			null
+		)
+	})
 
-        requests[0].respond(
-            404,
-            {"Content-Type": "application/json"},
-            ''
-        )
-    })
+	it('handle http errors', done => {
+		http('', {}).then(
+			value => {
+				expect(true).to.be.false
+				done()
+			},
+			(err) => {
+				expect(err).to.be.a('Error')
+				expect(err.status).to.be.a('Number')
+				expect(err.status).to.be.equal(404)
+				expect(err.body).to.be.a('String')
+				done()
+			}
+		).catch(err => done(err))
+
+		requests[0].respond(
+			404,
+			{'Content-Type': 'application/json'},
+			''
+		)
+	})
 })
