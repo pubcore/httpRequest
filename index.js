3192bbff17cda83aeaec66496e4a1aefb23437b4
Jochen Probst <jpr@dds-mbpro-150.fritz.box>
2020-03-20 22:38:43 +0100

    update dev dependencies

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
+exports["default"] = void 0;
 
-exports.default = function (uri, data) {
-    var method = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'POST';
+var _default = function _default(uri, data) {
+  var method = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'POST';
+  return new Promise(function (resolve, reject) {
+    var req = new XMLHttpRequest();
 
-    return new Promise(function (resolve, reject) {
-        var req = new XMLHttpRequest();
-        req.onreadystatechange = function () {
-            if (req.readyState != 4) {
-                return;
-            }
+    req.onreadystatechange = function () {
+      if (req.readyState != 4) {
+        return;
+      }
 
-            if (req.status != 200) {
-                var err = new Error('HTTP Error:' + req.statusText);
-                err.status = req.status;
-                try {
-                    err.body = typeof err.response == 'string' ? req.response : req.response && JSON.stringify(req.response) || req.responseText;
-                } catch (e) {
-                    err.body = '';
-                }
-                reject(err);
-            } else {
-                if (typeof req.response == 'string') {
-                    resolve(req.response && JSON.parse(req.response) || null);
-                } else if (req.response === null || req.response) {
-                    resolve(req.response);
-                } else {
-                    resolve(req.responseText && JSON.parse(req.responseText) || null);
-                }
-            }
-        };
+      if (req.status != 200) {
+        var err = new Error('HTTP Error:' + req.statusText);
+        err.status = req.status;
 
-        req.open(method, uri, true);
-        req.withCredentials = true;
-        req.setRequestHeader('Accept', 'application/json');
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.responseType = 'json';
-        req.send(JSON.stringify(data));
-    });
+        try {
+          err.body = typeof err.response == 'string' ? req.response : req.response && JSON.stringify(req.response) || req.responseText;
+        } catch (e) {
+          err.body = '';
+        }
+
+        reject(err);
+      } else {
+        if (typeof req.response == 'string') {
+          resolve(req.response && JSON.parse(req.response) || null);
+        } else if (req.response === null || req.response) {
+          resolve(req.response);
+        } else {
+          resolve(req.responseText && JSON.parse(req.responseText) || null);
+        }
+      }
+    };
+
+    req.open(method, uri, true);
+    req.withCredentials = true;
+    req.setRequestHeader('Accept', 'application/json');
+    req.setRequestHeader('Content-Type', 'application/json');
+    req.responseType = 'json';
+    req.send(JSON.stringify(data));
+  });
 };
+
+exports["default"] = _default;
